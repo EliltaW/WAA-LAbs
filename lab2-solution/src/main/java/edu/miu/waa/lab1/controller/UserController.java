c94d3c31d28bb892c72afa1d1bd1fe9d23f21438
EliltaW <eliltawondimu12@gmail.com>
2022-04-01 19:03:27 -0500

    duplicated file deleted

--- lab2-solution/src/main/java/edu/miu/waa/lab1/controller/UserController.java
+++
@@ -1,2 +0,0 @@
-package edu.miu.waa.lab1.controller;public class UserController {
-}
--- lab2-solution/src/main/java/edu/miu/waa/lab1/model/dto/Content.java
+++
@@ -1,8 +0,0 @@
-package edu.miu.waa.lab1.model.dto;
-
-import lombok.Data;
-
-@Data
-public class Content {
-String content;
-}
